ec60952cb20fb4456e0df6820e5fa7e4a52eb5f8
Sam Yerkes <sam@yrks.io>
2024-01-10 17:19:24 -0500

    Adds ability to create kubernetes yaml as a dry-run

--- lua/kubectl/init.lua
+++ lua/kubectl/init.lua
@@ -1,34 +1,140 @@
-local K = {}
+local kubectl = {}
 
-function K.setup(opts)
-	opts = opts or {}
+local function with_defaults(options)
+	return {
+		extension = options.extension or "yaml"
+	}
+end
 
-	local kubectlApply = function()
-		local buffer = vim.fn.expand("%")
-		if string.match(buffer, ".yaml$") or string.match(buffer, ".yml$") then
-			local cmd = "kubectl apply "
-			if string.match(buffer, "kustomization.yaml$") then
-				-- kustomizations can only be applied from the directory
-				-- so we need to remove the filename from the buffer
-				buffer = string.gsub(buffer, "kustomization.yaml$", "")
-				cmd = cmd .. "-k " .. buffer
-			else
-				cmd = cmd .. "-f " .. buffer
-			end
-			vim.cmd("!" .. cmd)
+-- This function is supposed to be called explicitly by users to configure this
+-- plugin
+function kubectl.setup(options)
+	-- avoid setting global values outside of this function. Global state
+	-- mutations are hard to debug and test, so having them in a single
+	-- function/module makes it easier to reason about all possible changes
+	kubectl.options = with_defaults(options)
+
+	-- do here any startup your plugin needs, like creating commands and
+	-- mappings that depend on values passed in options
+	vim.api.nvim_create_user_command("KubectlApply", kubectl.apply, {})
+	vim.api.nvim_create_user_command("KubectlCreate", kubectl.create, {})
+	vim.api.nvim_create_user_command("KubectlDelete", kubectl.delete, {})
+end
+
+function kubectl.is_configured()
+	return kubectl.options ~= nil
+end
+
+-- Apply a Kubernetes manifest
+function kubectl.apply()
+	if not kubectl.is_configured() then
+		return
+	end
+
+	local buffer = vim.fn.expand("%")
+	if string.match(buffer, ".yaml$") or string.match(buffer, ".yml$") then
+		local cmd = "kubectl apply "
+		if string.match(buffer, "kustomization.yaml$") then
+			-- kustomizations can only be applied from the directory
+			-- so we need to remove the filename from the buffer
+			buffer = string.gsub(buffer, "kustomization.yaml$", "")
+			cmd = cmd .. "-k " .. buffer
 		else
-			print("ERROR: Can not apply " .. buffer .. " because it does not end in .yaml or .yml")
+			cmd = cmd .. "-f " .. buffer
+		end
+		vim.cmd("!" .. cmd)
+	else
+		print("ERROR: Can not apply " .. buffer .. " because it does not end in .yaml or .yml")
+	end
+end
+
+-- Create a new Kubernetes object manifest
+function kubectl.create()
+	if not kubectl.is_configured() then
+		return
+	end
+
+	local args = {}
+	local title = "Creating a new Kubernetes object manifest"
+	local type = vim.fn.input(title .. "\ntype: ")
+	local name = vim.fn.input("name: ")
+	local command = ""
+	local cmd = "!kubectl "
+
+	if string.match(type, "po") then
+		cmd = cmd .. "run"
+	else
+		cmd = cmd .. "create " .. type
+	end
+
+	-- define the namespace for everything except namespaces
+	local namespace = ""
+	if type ~= "namespace" and type ~= "ns" then
+		namespace = vim.fn.input("namespace: ")
+	end
+	if namespace ~= "" then
+		cmd = cmd .. " --namespace=" .. namespace
+	end
+
+	-- define which flags are required per object type
+	local ktype = {}
+	ktype["cm"] = { {}, true }
+	ktype["configmap"] = { {}, true }
+	ktype["deployment"] = { { "image", "replicas", "port" }, false }
+	ktype["po"] = { { "image", "port" }, false }
+	ktype["pod"] = { { "image", "port" }, false }
+
+	-- add the individual flags to the command
+	if ktype[type] ~= nil then
+		for k, v in pairs(kubectl.flag(ktype[type][1], ktype[type][2])) do
+			table.insert(args, v)
 		end
 	end
 
-	vim.keymap.set('n', '<leader>ka', kubectlApply, { desc = '[k]ubectl [a]pply' })
-
-	local kubectlDelete = function()
-		local kubeObject = vim.fn.input("> kubectl delete ")
-		vim.cmd("!" .. "kubectl delete " .. kubeObject)
+	-- pods can have an additional command on the end of the run
+	-- but it has to come after the dry-run and output flags
+	if string.match(type, "po") then
+		command = vim.fn.input("command: ")
+		command = " -- " .. command
 	end
 
-	vim.keymap.set('n', '<leader>kd', kubectlDelete, { desc = '[k]ubectl [d]elete' })
+	local filename = vim.fn.input("filename: ")
+
+	cmd = cmd .. " " .. name .. " " ..
+	    table.concat(args, " ") .. " --dry-run=client -o yaml > " .. filename .. command
+
+	print(cmd)
+	vim.cmd(cmd)
 end
 
-return K
+function kubectl.flag(flags, freeformOpts)
+	local args = {}
+	for _, flag in ipairs(flags) do
+		local value = vim.fn.input(flag .. ": ")
+		if value ~= "" then
+			local myflag = "--" .. flag .. "=" .. value
+			table.insert(args, myflag)
+		end
+	end
+	if freeformOpts then
+		local extraflags = vim.fn.input("additional flags: ")
+		table.insert(args, extraflags)
+	end
+	return args
+end
+
+-- Delete a Kubernetes object
+function kubectl.delete()
+	local title = "WARNING: This will delete the object from the cluster"
+	local type = vim.fn.input(title .. "\ntype: ")
+	local object = vim.fn.input("name: ")
+	vim.cmd("!" .. "kubectl delete " .. type .. " " .. object)
+end
+
+-- Key mappings
+vim.keymap.set('n', '<leader>ka', kubectl.apply, { desc = '[k]ubectl [a]pply' })
+vim.keymap.set('n', '<leader>kc', kubectl.create, { desc = '[k]ubectl [c]reate' })
+vim.keymap.set('n', '<leader>kd', kubectl.delete, { desc = '[k]ubectl [d]elete' })
+
+kubectl.options = nil
+return kubectl
--- plugin/kubectl.lua
+++ plugin/kubectl.lua
@@ -1,1 +1,20 @@
+if vim.fn.has("nvim-0.7.0") == 0 then
+	vim.api.nvim_err_writeln("kubectl requires at least nvim-0.7.0.1")
+	return
+end
 
+-- make sure this file is loaded only once
+if vim.g.loaded_kubectl == 1 then
+	return
+end
+vim.g.loaded_kubectl = 1
+
+-- create any global command that does not depend on user setup
+-- usually it is better to define most commands/mappings in the setup function
+-- Be careful to not overuse this file!
+local kubectl = require("kubectl")
+
+vim.api.nvim_create_user_command(
+	"KubectlApply",
+	kubectl.apply,
+	{})
